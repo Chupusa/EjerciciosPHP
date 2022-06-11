cfa5034b4f4c4dda6e95a75c3442d402c7fddad4
Jesus Urbina-LapLic <jsidereus@gmail.com>
2022-06-11 13:08:04 -0500

    Se agrega archo Menores.php

---
+++ Menores.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+<title>Ejemplo de PHP</title>
+</head>
+
+<body>
+<font face="Arial, Helvetica, sans-serif">
+
+<?php 
+    $N=$_GET['N'];
+    print "Los numeros impares menores de $N son: ";
+    $i=0;
+    while ($i<$N){
+          $resto=$i%2;
+          if ($resto == 1){
+              print "$i ";
+           }
+          $i++;
+    }
+?>
+</font>
+</body>
+</html>
