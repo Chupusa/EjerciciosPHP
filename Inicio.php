6f1dd7cc46d7d24da8ca7c13a0388165d674ed49
Jesus Urbina-LapLic <jsidereus@gmail.com>
2022-06-11 14:15:34 -0500

    Se modifica el Comentario en Inicio.php

--- Inicio.php
+++ Inicio.php
@@ -4,5 +4,5 @@
    echo 'Chupusa hace su Primer Programa'."<br>";
    echo 'Linea 2'."<br>";
    echo 'Otra Linea la 3'."<br>";
-   //Este es otro comentario
+   //Este es otro comentario lo quito o no lo quito
 ?>
